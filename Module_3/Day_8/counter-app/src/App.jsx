6f367eedae89a1f28ce3f95ee2f3b48a6207ffd0
khushi <khushi112200311@gmail.com>
2025-12-17 23:08:37 +0530

    Add reusable MessageCard component with props (Q3)

--- Module_3/Day_8/counter-app/src/App.jsx
+++ Module_3/Day_8/counter-app/src/App.jsx
@@ -1,11 +1,32 @@
 import Counter from "./Counter";
 import Calculator from "./Calculator";
+import MessageCard from "./MessageCard";
 
 function App() {
   return (
     <div>
       <Counter />
       <Calculator />
+
+      <MessageCard
+        title="Welcome"
+        message="This is the first message card."
+      />
+
+      <MessageCard
+        title="React Props"
+        message="Props allow data to be passed to components."
+      />
+
+      <MessageCard
+        title="Reusable Component"
+        message="This card component is reused multiple times."
+      />
+
+      <MessageCard
+        title="Assignment Complete"
+        message="Q3 implemented successfully."
+      />
     </div>
   );
 }
