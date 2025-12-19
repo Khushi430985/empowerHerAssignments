eac9b998278655077abe0de2d9e3616fbac35c3a
khushi <khushi112200311@gmail.com>
2025-12-19 22:36:53 +0530

    user-data

---
+++ Module_3/Day_9/user-data-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UserData from "./UserData";
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Fetch and Display User Data</h2>
+      <UserData />
+    </div>
+  );
+}
+
+export default App;
