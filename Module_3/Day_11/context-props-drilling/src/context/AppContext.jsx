7dbc2c3f6b16f416cc7748d5deb7373cf4d1f37a
khushi <khushi112200311@gmail.com>
2025-12-23 22:33:15 +0530

    prop drilling context api

---
+++ Module_3/Day_11/context-props-drilling/src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+
+export const AppContext = createContext();
+
+const AppContextProvider = ({ children }) => {
+  const values = {
+    a: "Apple",
+    b: "Ball",
+    c: "Cat",
+    d: "Dog",
+    e: "Elephant",
+    f: "Fish"
+  };
+
+  return (
+    <AppContext.Provider value={values}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export default AppContextProvider;
