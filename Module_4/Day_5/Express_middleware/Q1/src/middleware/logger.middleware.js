6f32262f19f435be00bab602c19fad5524a53e64
Khushi430985 <khushi112200311@gmail.com>
2026-01-20 23:18:08 +0530

    Add logger middleware for request logging

---
+++ Module_4/Day_5/Express_middleware/Q1/src/middleware/logger.middleware.js
@@ -0,0 +1,5 @@
+export default function loggerMiddleware(req, res, next) {
+  const time = new Date().toISOString().replace("T", " ").split(".")[0];
+  console.log(`[${time}] ${req.method} ${req.originalUrl}`);
+  next();
+}
